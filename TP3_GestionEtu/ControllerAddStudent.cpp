b71dbedfd3022d4cdcf043de4b82af4ee5fdfab8
IUTABGDIN105-01\Administrateur <iutbourg.smir@univ-lyon1.fr>
2022-11-21 13:47:05 +0100

    Validation finale

--- TP3_GestionEtu/ControllerAddStudent.cpp
+++ TP3_GestionEtu/ControllerAddStudent.cpp
@@ -17,5 +17,5 @@ ControllerAddStudent::ControllerAddStudent(Promotion* promotion) : promotion(pro
  */
 
 void ControllerAddStudent::control(QVector<QString> student) {
-	promotion->add(new Student(student[0], student[1], student[2], student[3], student[4]));
+	promotion->add(new Student(student[0], student[1], student[2], student[4], student[3]));
 }
